25acc8e16fe06adfbc371a47665646e8385e34c8
Andru Cherny <wiroatom@gmail.com>
2025-05-20 17:09:06 +0300

    Add logging

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"k8s.io/client-go/kubernetes"
 	"os"
@@ -11,7 +12,7 @@ import (
 	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/cmd"
@@ -33,6 +34,10 @@ func main() {
 		panic("GROUP_NAME must be specified")
 	}
 
+	flag.Set("v", "2")
+	flag.Parse()
+
+	klog.Infof("Starting webhook server with group name: %s", GroupName)
 	cmd.RunWebhookServer(GroupName, &customDNSProviderSolver{})
 }
 
@@ -58,30 +63,40 @@ func (c *customDNSProviderSolver) Name() string {
 }
 
 func (c *customDNSProviderSolver) DoDNSAPI(action string, ch *v1alpha1.ChallengeRequest) error {
+	klog.Infof("Starting DoDNSAPI with action=%s for domain=%s", action, ch.ResolvedFQDN)
+
 	cfg, err := loadConfig(ch.Config)
 	if err != nil {
-		klog.Errorf("Failed to log config %v: %v", ch.Config, err)
+		klog.Errorf("Failed to parse config: %v", err)
 		return err
 	}
 
+	klog.Infof("Loading secret %s/%s", cfg.EnvSecretRef.Namespace, cfg.EnvSecretRef.Name)
 	envSecret, err := c.client.CoreV1().Secrets(cfg.EnvSecretRef.Namespace).Get(context.TODO(), cfg.EnvSecretRef.Name, metav1.GetOptions{})
 	if err != nil {
+		klog.Errorf("Failed to get secret: %v", err)
 		return err
 	}
 
 	envData, ok := envSecret.Data["env"]
 	if !ok {
+		klog.Errorf("Missing 'env' key in secret")
 		return fmt.Errorf("no env in secret")
 	}
 
 	env := envFromSecret{}
 	if err := json.Unmarshal(envData, &env); err != nil {
+		klog.Errorf("Failed to unmarshal env data: %v", err)
 		return err
 	}
 
 	uuid := uuid.New()
 	stdoutFile, err := os.CreateTemp("/tmp", uuid.String())
 	defer os.Remove(stdoutFile.Name())
+	if err != nil {
+		klog.Errorf("Failed to create temp file: %v", err)
+		return err
+	}
 
 	procAttr := &os.ProcAttr{
 		Files: []*os.File{os.Stdin, stdoutFile, os.Stderr},
@@ -90,14 +105,16 @@ func (c *customDNSProviderSolver) DoDNSAPI(action string, ch *v1alpha1.Challenge
 
 	dir, err := os.Getwd()
 	if err != nil {
-		klog.Errorf("Can not get process`: %v", err)
+		klog.Errorf("Failed to get working directory: %v", err)
 		return err
 	}
 
+	klog.Infof("Executing %s with action=%s", acmeDelegate, action)
 	process, err := os.StartProcess(dir+acmeDelegate, []string{
 		dir + acmeDelegate, cfg.DNSAPI, action, util.UnFqdn(ch.ResolvedFQDN), ch.Key,
 	}, procAttr)
 	if err != nil {
+		klog.Errorf("Failed to start process: %v", err)
 		return err
 	}
 
@@ -106,17 +123,18 @@ func (c *customDNSProviderSolver) DoDNSAPI(action string, ch *v1alpha1.Challenge
 
 	outFile, err := os.Open(stdoutFile.Name())
 	if err != nil {
+		klog.Errorf("Failed to read output file: %v", err)
 		return err
 	}
 
 	output := make([]byte, 1048576)
 	count, err := outFile.Read(output)
 	if err != nil {
+		klog.Errorf("Failed to read output content: %v", err)
 		return err
 	}
 
-	os.Stdout.WriteString(string(output) + "\n")
-	os.Stdout.WriteString("[ACME] read output count=" + fmt.Sprint(count) + "\n")
+	klog.Infof("Process output (%d bytes): %s", count, string(output))
 	lines := strings.Split(string(output), "\n")
 
 	retval := "0"
@@ -128,24 +146,29 @@ func (c *customDNSProviderSolver) DoDNSAPI(action string, ch *v1alpha1.Challenge
 	}
 
 	if retval == "0" {
+		klog.Infof("ACME script succeeded for domain=%s", ch.ResolvedFQDN)
 		return nil
 	}
 
+	klog.Warningf("ACME script returned failure: %s", retval)
 	return fmt.Errorf("failed to run acme.sh, error=%s ", retval)
 }
 
 func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+	klog.Infof("Presenting DNS challenge for %s", ch.ResolvedFQDN)
 	return c.DoDNSAPI("add", ch)
 }
 
 func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
+	klog.Infof("Cleaning up DNS challenge for %s", ch.ResolvedFQDN)
 	return c.DoDNSAPI("rm", ch)
 }
 
 func (c *customDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+	klog.Infof("Initializing Kubernetes client")
 	cl, err := kubernetes.NewForConfig(kubeClientConfig)
 	if err != nil {
-		klog.Errorf("Failed to new kubernetes client: %v", err)
+		klog.Errorf("Failed to create Kubernetes client: %v", err)
 		return err
 	}
 	c.client = cl
@@ -156,11 +179,13 @@ func loadConfig(cfgJSON *extapi.JSON) (customDNSProviderConfig, error) {
 	ttl := uint64(defaultTTL)
 	cfg := customDNSProviderConfig{TTL: &ttl}
 	if cfgJSON == nil {
+		klog.Infof("No config JSON provided; using default TTL=%d", defaultTTL)
 		return cfg, nil
 	}
 	if err := json.Unmarshal(cfgJSON.Raw, &cfg); err != nil {
+		klog.Errorf("Failed to unmarshal solver config: %v", err)
 		return cfg, fmt.Errorf("error decoding solver config: %v", err)
 	}
-
+	klog.Infof("Loaded config: DNSAPI=%s, TTL=%d", cfg.DNSAPI, *cfg.TTL)
 	return cfg, nil
 }
